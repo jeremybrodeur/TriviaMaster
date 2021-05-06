9022895d8b5622320ee7bf00f0c95c87dbc8bb31
jeremybrodeur <jibro30@hotmail.com>
2021-05-06 17:27:21 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,229 @@
+openNav();
+var btn = document.querySelector('#loadQuestion');
+var btnValidator = document.querySelector('#validator');
+var btnNext = document.querySelector('#nextQuestion');
+var divPointer = document.querySelector('#questionBox');
+var questionChoice1 = document.querySelector('label[for="1"]');
+var questionChoice2 = document.querySelector('label[for="2"]');
+var questionChoice3 = document.querySelector('label[for="3"]');
+var questionChoice4 = document.querySelector('label[for="4"]');
+var radioButton1 = document.getElementById('1');
+var radioButton2 = document.getElementById('2');
+var radioButton3 = document.getElementById('3');
+var radioButton4 = document.getElementById('4');
+var score = 0;
+var scoreCounter = document.getElementById('scoreCounter').innerHTML;
+console.log(scoreCounter);
+console.log(document.querySelectorAll('input[type="radio"]'));
+btn.onclick = function(){
+
+    btnValidator.style.display = 'block';
+    btnNext.style.display = 'none';
+    var ourRequest = new XMLHttpRequest();
+    let category ="";
+    let categorySelect = document.querySelector('#categorySelecter').value;
+    if(categorySelect != 'any'){
+        category ="&category=" + categorySelect;
+    }
+        ourRequest.open('GET','https://opentdb.com/api.php?amount=1&type=multiple'+ category);
+    
+
+        ourRequest.onload = function(){
+            if (ourRequest.status>= 200 && ourRequest.status < 400) {
+                var ourData = JSON.parse(ourRequest.responseText);
+                renderHTML(ourData);
+                document.querySelector('.card').style.display = 'block';
+            }
+            else{
+               alert("Connection lost.");
+            }
+        };
+        ourRequest.send();
+};
+btnNext.onclick = function(){
+
+    var ourRequest = new XMLHttpRequest();
+    let category ="";
+    let categorySelect = document.querySelector('#categorySelecter').value;
+    if(categorySelect != 'any'){
+        category ="&category=" + categorySelect;
+    }
+        ourRequest.open('GET','https://opentdb.com/api.php?amount=1&type=multiple'+ category);
+    
+
+        ourRequest.onload = function(){
+            if (ourRequest.status>= 200 && ourRequest.status < 400) {
+                var ourData = JSON.parse(ourRequest.responseText);
+                renderHTML(ourData);
+                document.querySelector('.card').style.display = 'block';
+            }
+            else{
+               alert("Connection lost.");
+            }
+        };
+        ourRequest.send();
+        btnValidator.style.display = 'block';
+        btnNext.style.display = 'none';
+};
+function renderHTML(data){
+    resetCheck();
+    resetStyle();
+    var question = "";
+    var choice1 = "";
+    var choice2 = "";
+    var choice3 = "";
+    var choice4 = "";
+    var indexGenerator = Math.floor(Math.random() * 3);
+    console.log(indexGenerator);
+    var tabAnswers = data.results[0].incorrect_answers;
+    tabAnswers.splice(indexGenerator,0,data.results[0].correct_answer);
+    question = data.results[0].question;
+    console.log(tabAnswers);
+    for (let i = 0; i < tabAnswers.length; i++) {
+        choice1 = tabAnswers[0];
+        choice2 = tabAnswers[1];
+        choice3 = tabAnswers[2];
+        choice4 = tabAnswers[3];
+        
+    }
+    divPointer.innerHTML = question;
+    questionChoice1.innerHTML = choice1;
+    questionChoice2.innerHTML = choice2;
+    questionChoice3.innerHTML = choice3;
+    questionChoice4.innerHTML = choice4;
+    var tabReponse = [questionChoice1,questionChoice2,questionChoice3,questionChoice4];
+    btnValidator.onclick = function(){
+        if(radioButton1.checked ||radioButton2.checked ||radioButton3.checked ||radioButton4.checked){
+        validateAnswer(data.results[0].correct_answer);
+        for (let i = 0; i < tabReponse.length; i++) {
+            if(tabReponse[i].innerHTML==tabAnswers[indexGenerator]){
+                tabReponse[i].style.color='green';
+                tabReponse[i].style.fontWeight='800';
+            }   
+        } 
+    }
+        else alert('Please select a value!');
+        checkIfWin(score);
+    };  
+}
+
+function validateAnswer(answer) {
+    
+    if(document.getElementById('1').checked){
+        if(questionChoice1.innerHTML == answer){
+            questionChoice1.style.color = 'green';
+            questionChoice1.style.fontWeight = '800';
+            score++;
+            $('#scoreCounter').html('Current score : '+ score); 
+        }
+        else{
+            questionChoice1.style.color = 'red';
+            questionChoice1.style.fontWeight = '800';
+        }
+    }
+    if(document.getElementById('2').checked){
+        if(questionChoice2.innerHTML == answer){
+            questionChoice2.style.color = 'green';
+            questionChoice2.style.fontWeight = '800';
+            score++;
+            $('#scoreCounter').html('Current score : '+ score);
+            
+        }
+        else{
+            questionChoice2.style.color = 'red';
+            questionChoice2.style.fontWeight = '800';
+        }
+    }
+    if(document.getElementById('3').checked){
+        if(questionChoice3.innerHTML == answer){
+            questionChoice3.style.color = 'green';
+            questionChoice3.style.fontWeight = '800';
+            score++;
+            $('#scoreCounter').html('Current score : '+ score);
+            
+        }
+        else{
+            questionChoice3.style.color = 'red';
+            questionChoice3.style.fontWeight = '800';
+        }
+    }
+    if(document.getElementById('4').checked){
+        if(questionChoice4.innerHTML == answer){
+            questionChoice4.style.color = 'green';
+            questionChoice4.style.fontWeight = '800';
+            score++;
+            $('#scoreCounter').html('Current score : '+ score);
+            
+        }
+        else{
+            questionChoice4.style.color = 'red';
+            questionChoice4.style.fontWeight = '800';
+        }
+        
+    }
+    btnValidator.style.display = 'none';
+    btnNext.style.display = 'block';  
+}
+function resetStyle(){
+    questionChoice1.style.color='#000000';
+    questionChoice1.style.fontWeight ='400';
+    questionChoice2.style.color='#000000';
+    questionChoice2.style.fontWeight ='400';
+    questionChoice3.style.color='#000000';
+    questionChoice3.style.fontWeight ='400';
+    questionChoice4.style.color='#000000';
+    questionChoice4.style.fontWeight ='400';
+}
+
+function resetCheck() {
+    document.querySelectorAll('input[type="radio"]').forEach(function (elm,i) {
+        elm.checked = false;
+    });
+}
+function checkIfWin(score){
+    if(score == 15){
+        
+        $("#myNav2").animate({
+            opacity: '1',
+            zIndex: '1'
+            
+        },100);
+        $("body").css({
+            zIndex: '0'
+            
+        });
+        $("#myNav2").css({
+            display: 'block'
+            
+        });
+        openNav2();
+    }
+}
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  function openNav2() {
+    document.getElementById("myNav2").style.width = "100%";
+  }
+$('#startGame').click(function() {
+    $("#myNav").fadeOut();
+    $("body").css({
+        zIndex: '1'
+        
+    });
+    $("#myNav").css({
+        display: 'none'
+        
+    });
+    $("#myNav2").css({
+        display: 'none'
+        
+    });
+
+});
+
+
+
+
+
+
